668c0302da3830c3ddd9880437f4ac5da1fbb209
Abdullah <abdulhaseeb9274@gmail.com>
2024-07-20 15:49:58 +0500

    properly react-render-if-visible applied, updated loading component, etc

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,24 +3,18 @@
 import { useRef } from "react";
 import Header from "../Header/Header";
 import StickyCursor from "../StickyCursor/StickyCursor";
-// import HeroSection from "../HeroSection/HeroSection";
-// import BentoGrid from "../BentoGrid/BentoGrid";
-// import WhatAllAbout from "../WhatAllAbout/WhatAllAbout";
-// import WordpressToReact from "../WordpressToReact/WordpressToReact";
-// import TechUsed from "../TechUsed/TechUsed";
-// import Security from "../Security/Security";
-// import Section7 from "../Section7/Section7";
-// import WayTooManySystemsMain from "../WayToManySystems/WayTooManySystemsMain";
-// import Section9 from "../Section9/Section9";
-// import Contact from "../Contact/Contact";
-// import GameSectionMain from "../GameSection/GameSectionMain";
 import dynamic from "next/dynamic";
 import { useTranslations } from "next-intl";
 import RenderIfVisible from "react-render-if-visible";
 
 const loadingComponent = () => (
-  <div className=" w-full h-screen grid place-items-center">
-    <div className="spinner-medium"></div>
+  <div className=" grid p-4 md:px-24 md:py-12 grid-cols-3 grid-rows-3 gap-6 w-full h-screen animate-pulse">
+    <div className=" w-full h-full rounded-2xl bg-gray-300 col-span-1 row-span-2 "></div>
+    <div className=" w-full h-full rounded-2xl bg-gray-300 col-span-2 row-span-1 "></div>
+    <div className=" w-full h-full rounded-2xl bg-gray-300 col-span-1 row-span-1 "></div>
+    <div className=" w-full h-full rounded-2xl bg-gray-300 col-span-1 row-span-1 "></div>
+    <div className=" w-full h-full rounded-2xl bg-gray-300 col-span-2 row-span-1 "></div>
+    <div className=" w-full h-full rounded-2xl bg-gray-300 col-span-1 row-span-1 "></div>
   </div>
 );
 
@@ -72,43 +66,36 @@ export default function Main({}) {
   const footerTrans = useTranslations("footer");
   const currentYear = new Date().getFullYear();
 
+  const allComponents = [
+    <HeroSection />,
+    <BentoGrid />,
+    <WhatAllAbout />,
+    <WordpressToReact />,
+    <TechUsed />,
+    <Security />,
+    <Section7 />,
+    <div className=" relative">
+      <WayTooManySystemsMain />
+    </div>,
+    <Section9 />,
+    <div className=" hidden sm:block">
+      <GameSectionMain />
+    </div>,
+    <Contact />,
+    <footer className=" py-4 w-full">
+      <p className=" text-center">
+        {currentYear} &copy; {footerTrans("text")}
+      </p>
+    </footer>,
+  ];
   return (
     <div>
       <Header ref={stickyElement} />
 
       <div className={" bg-white grid relative "}>
-        
-        <RenderIfVisible>
-          <HeroSection />
-
-          <BentoGrid />
-
-          <WhatAllAbout />
-
-          <WordpressToReact />
-
-          <TechUsed />
-
-          <Security />
-
-          <Section7 />
-        </RenderIfVisible>
-
-        <WayTooManySystemsMain />
-
-        <RenderIfVisible>
-          <Section9 />
-          <div className=" hidden sm:block">
-            <GameSectionMain />
-          </div>
-          <Contact />
-          <footer className=" py-4 w-full">
-            <p className=" text-center">
-              {currentYear} &copy; {footerTrans("text")}
-            </p>
-          </footer>
-        </RenderIfVisible>
-
+        {allComponents.map((item, index) => {
+          return <RenderIfVisible key={index}>{item}</RenderIfVisible>;
+        })}
       </div>
 
       {/* <div className=" hidden sm:block">
