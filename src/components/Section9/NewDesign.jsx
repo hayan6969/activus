d8f0be55ed9c823e0102cbc778af883e3757a4a9
Abdullah <abdulhaseeb9274@gmail.com>
2024-07-26 23:15:21 +0500

    design in app dev corrected

--- src/components/Section9/NewDesign.jsx
+++ src/components/Section9/NewDesign.jsx
@@ -7,9 +7,9 @@ export default function NewDesign() {
   return (
     <>
       <input id="switch" type="checkbox" />
-      <div className="app">
-        <div className="body">
-          <div className="phone w-[25rem] md:w-[19rem] h-[17rem] ">
+      <div className="app ">
+        <div className="body ">
+          <div className="phone w-[25rem] md:w-[19rem] h-[17rem]">
             <div className="menu">
               <div className="time">4:20</div>
               <div className="icons">
@@ -17,13 +17,13 @@ export default function NewDesign() {
                 <div className="battery"></div>
               </div>
             </div>
-
-            <div className="content">
-              <div className="circle w-[8rem] md:w-[5rem] h-[8rem] md:h-[5rem] ">
-                <div className="crescent w-[6.75rem] md:w-[4.3rem] h-[6.75rem] md:h-[4.3rem]"></div>
+{/* 5 & 4.3 */}
+            <div className="content ">
+              <div className="circle w-[8rem] md:w-[3rem] h-[8rem] md:h-[3rem] ">
+                <div className="crescent w-[6.75rem] md:w-[2.6rem] h-[6.75rem] md:h-[2.6rem]"></div>
               </div>
 
-              <label htmlFor="switch">
+              <label className={"label-mb"} htmlFor="switch">
                 <div className="toggle-mb"></div>
                 <div className="names">
                   <p className="light">Light</p>
